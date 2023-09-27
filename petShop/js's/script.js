bcac8421d25b43d3e1fe01c340eb21a8d81c4ccb
matheusviniciusbrito <matheus.vinicius.mvrb@gmail.com>
2023-09-26 21:43:48 -0300

    Update comk paginas, sobre nos, contato...

--- petShop/js's/script.js
+++ petShop/js's/script.js
@@ -19,6 +19,14 @@ const toggleBtn = document.querySelector('.toggle_btn')
 const toggleBtnIcon = document.querySelector('.toggle_btn i')
 const dropDownMenu = document.querySelector('.dropdown_menu')
 
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
 function alternarClasse(elementoClicado) {
     var imagens = document.querySelectorAll('.imagens .img-sup img');
     for (var i = 0; i < imagens.length; i++) {
